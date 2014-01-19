34dca99bd84459e15f359f49ac165d04f71f6772
Mzuj <your_email@youremail.com>
2014-01-19 19:54:33 +0100

    sessionState=inProc, filter removed

--- MVCCourier/App_Start/FilterConfig.cs
+++ MVCCourier/App_Start/FilterConfig.cs
@@ -7,7 +7,7 @@ namespace MVCCourier
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
-            filters.Add(new HandleErrorAttribute());
+            //filters.Add(new HandleErrorAttribute());
         }
     }
 }
